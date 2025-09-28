ea5a27d9f8a839d4651f77272d679600add0a900
handy <handy.kang.dev@gmail.com>
2025-09-28 22:46:07 +0900

    feat: Tailwind CSS 및 Vite 설정 업데이트
    - index.html에서 다크 클래스 제거
    - package.json 및 package-lock.json에 lodash 및 관련 패키지 추가
    - vite.config.ts에 rollup-plugin-visualizer 및 vite-plugin-remove-console 추가
    - App.tsx에서 BuiltIn 및 Env 컴포넌트 추가
    - index.css에 새로운 색상 및 애니메이션 추가

--- 16_tailwind_theme/src/App.tsx
+++ 16_tailwind_theme/src/App.tsx
@@ -1,15 +1,39 @@
 import "./App.css";
-import ToggleTheme from "./component/ToggleTheme";
 
 function App() {
   return (
     <>
-      <div className="flex justify-between items-center mb-8 flex-col">
-        <h1 className="text-3xl font-bold">Tailwind Dark Theme</h1>
-        <ToggleTheme />
+      <div>
+        <BuiltIn />
+        <hr />
+        <Env />
       </div>
     </>
   );
 }
 
 export default App;
+
+const BuiltIn = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold">Built-in</h2>
+      <p>Current Mode: {import.meta.env.MODE}</p>
+      <p>Base URL: {import.meta.env.BASE_URL}</p>
+      <p>Is Production: {import.meta.env.PROD ? "Yes" : "No"}</p>
+      <p>Is Development: {import.meta.env.DEV ? "Yes" : "No"}</p>
+      <p>Is Server-Side Rendering: {import.meta.env.SSR ? "Yes" : "No"}</p>
+    </div>
+  );
+};
+
+const Env = () => {
+  console.log("컴포넌트 랜더링");
+  return (
+    <div>
+      <h2 className="text-2xl font-bold">Env</h2>
+      <p>VITE_SAMPLE_KEY: {import.meta.env.VITE_SAMPLE_KEY}</p>
+      <p>DB_SECRET_KEY: {import.meta.env.DB_SECRET_KEY}</p>
+    </div>
+  );
+};
--- 16_tailwind_theme/vite.config.ts
+++ 16_tailwind_theme/vite.config.ts
@@ -1,8 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
+import removeConsole from "vite-plugin-remove-console";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), visualizer(), removeConsole()],
+  build: {
+    minify: "terser",
+    cssMinify: true,
+  },
 });
